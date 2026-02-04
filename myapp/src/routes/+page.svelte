0f7d6951d439eac0572f47f45a336cad744c1c7f
JoaoVictor_Moraes <joaovictor.mroaes2728@gmail.com>
2026-02-04 17:46:26 -0300

    feat/preparando ambiente svelte para começar a codificar

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { goto } from "$app/navigation";
+    import src from "$lib/assets/search.png"
+</script>
+
+<header></header>
+<main></main>
+<footer></footer>
+
+<style></style>
